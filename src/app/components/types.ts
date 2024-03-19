b4c1fa3b9b3d3fc9bd217b0be28a71e26a90321f
AbdelkerimMahfoudh <abdelkerim.mahfoudh@gmail.com>
2024-03-19 22:28:37 +0500

    types.ts creation

---
+++ src/app/components/types.ts
@@ -0,0 +1,24 @@
+
+interface Product {
+    id: number;
+    images: string[];
+    name: string;
+    description: string;
+    price: number;
+    contacts: string;
+    technicalCharacteristics?: TechnicalCharacteristics;
+    options: Option[];
+  }
+  
+  interface TechnicalCharacteristics {
+    carId: number;
+    brand: string;
+    model: string;
+    productionYear: number;
+    body: string;
+    mileage: number;
+  }
+  
+  interface Option {
+    optionName: string;
+  }
